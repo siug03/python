5d9574c8ae648472bf3e5b0b38507e50df8d53c5
siug03 <siug4549@gmail.com>
2022-09-03 16:27:30 +0900

    09/03 김시우 1차 완료

--- python_basics/02_data_if.py
+++ python_basics/02_data_if.py
@@ -8,8 +8,19 @@ while status:
     for word in ban_dict:
         if word in chat:
             bad_words += 1
-        if chat == "꺼져":
-                status = False
+    if chat == "꺼져":
+        status = False
+    if chat == "추가":
+        add_word = input("추가 금칙어 말해: ")
+        if add_word in ban_dict:
+            print(f"{add_word} 단어가 이미 있어")
+        else:
+            ban_dict.append(add_word)
+            print(f"{add_word} 단어가 금칙어 사전에 등록됨")
+        continue
+
+
+            
     if bad_words == 0:
         print("맞아.")
     else:
---
+++ python_basics/03_calculator.py
@@ -0,0 +1,21 @@
+number_a = 0
+symbol = 0
+number_b = 0
+input_data = 0
+
+number_a = int(input("숫자 A를 입력해"))
+symbol = input("+, -, *, / 중 하나를 입력해 :")
+number_b = int(input("숫자 B를 입력해"))
+
+
+if symbol == "+":
+    result = number_a + number_b
+elif symbol == "-":
+    result = number_a - number_b
+elif symbol == "*":
+    result = number_a * number_b
+elif symbol == "/":
+    result = number_a / number_b
+
+
+print(f"{number_a} {symbol} {number_b} = {result}")
