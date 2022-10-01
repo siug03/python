5aa3a29dc9150864fd0328744b7ca850fb7c7aa9
siug03 <siug4549@gmail.com>
2022-10-01 15:54:30 +0900

    10/1 로또 추첨기 제작

---
+++ python_basics/06_lotto.py
@@ -0,0 +1,20 @@
+from random import random
+
+
+import random
+
+
+
+lotto_numbers = []
+pick = 0
+lotto_picks = []
+
+for i in range(45):
+    lotto_numbers.append(i + 1)
+
+for j in range(7):
+    pick = lotto_numbers.pop(random.randint(0, len(lotto_numbers) - 1))
+    lotto_picks.append(pick)
+
+
+print(f"이번주 당첨 예상 번호는 {lotto_picks} 입니다.")
